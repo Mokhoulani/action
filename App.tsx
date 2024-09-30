0ec07720a723b8467cc80157155efc523c782db2
Mokhoulani <mohammad.ko555@gmail.com>
2024-09-30 10:54:10 +0200

    test

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text>Home !!</Text>
       <StatusBar style="auto" />
     </View>
   );
